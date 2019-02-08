6c565669875b1af3fa615496c151ba16aa159179
Pacôme <pacome.lehoux@laposte.net>
2019-02-08 14:56:40 +0100

    cleaned up main.cpp a bit

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,20 @@
     e-mail               : pacome.lehoux@insa-lyon.fr et mathieu.montgomery@insa-lyon.fr
 *************************************************************************/
 
-#include "main.h"
-#include "Parser.h"
-#include "Graph.h"
+#include <algorithm>
+#include <cctype>
+#include <iomanip>
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <iomanip>
 #include <map>
-#include <algorithm>
+#include <sstream>
 #include <string.h>
 #include <string>
 #include <unistd.h>
-#include <cctype>
+
+#include "main.h"
+#include "Graph.h"
+#include "Parser.h"
 
 using namespace std;
 
@@ -34,17 +35,9 @@ const char* USAGE_MESSAGE =
         "\t\t-e : exclude files based on their ending. Blacklisted file endings are stored in the x file.\n\n" //TODO put name of blacklist file
         "\t\t-t <hour> : only consider requests that occurred between <hour> (included)\n"
         "\t\t\tand <hour> + 1 (excluded). <hour> must be a number between 0 and 23.";
-
 const string LOCAL_URL("intranet-if.insa-lyon.fr");
 const string EXTENSIONS_BANNED("banned_extension.txt");
 
-constexpr int ARGS_NUM = 3;
-const char* VALID_ARGS[ARGS_NUM] = { "-g", "-e", "-t" };
-
-bool cmp (pair<string, int> const & a, pair<string, int> const & b)
-{
-	return (a.second > b.second);
-}
 
 int main(int argc, char ** argv)
 // Algorithme :
@@ -56,7 +49,8 @@ int main(int argc, char ** argv)
     if(checkCmdLine(argc, argv, args))
     {
         Parser* p;
-        try {
+        try
+        {
 		    p = makeParser(&args);
 
 		    if(p != nullptr)
@@ -66,13 +60,13 @@ int main(int argc, char ** argv)
             }
 
 		    delete p;
+            return 0;
         }
-        catch (FileNotFoundError &e) {
+        catch (FileNotFoundError &e)
+        {
             cerr << e.what() << endl;
             return -1;
         }
-
-        return 0;
     }
     
 	return -1;
@@ -92,35 +86,27 @@ Parser * makeParser(Args * argum)
 		parser = new Parser( argum ->inputFileName, to_string(argum->hour), to_string(argum->hour+1));
 	}
 
-	if ( argum->blacklistFiles ){
-	    try
-	    {
-			fstream extension;
-			string line;
-			extension.open(EXTENSIONS_BANNED);
+	if ( argum->blacklistFiles )
+	{
+        fstream extension;
+        string line;
+        extension.open(EXTENSIONS_BANNED);
 
-			bool haveExtensions = false;
-			while( ! extension.eof() )
-			{
-				getline(extension, line);
-				if( line.size( ) != 0 )
-				{
-					haveExtensions = true;
-					parser -> addBlacklist(line);
-				}
-			}
+        bool haveExtensions = false;
+        while( ! extension.eof() )
+        {
+            getline(extension, line);
+            if( line.size( ) != 0 )
+            {
+                haveExtensions = true;
+                parser -> addBlacklist(line);
+            }
+        }
 
-			if( ! haveExtensions )
-			{
-				cerr << "Le fichier banned_extension.txt ne contient aucune extensions à bannir" << endl;
-			}
-
-	    }
-	    catch(FileNotFoundError& e)
-	    {
-	        cerr << e.what() << endl;
-	        return nullptr;
-	    }
+        if( ! haveExtensions )
+        {
+            cerr << "Le fichier banned_extension.txt ne contient aucune extensions à bannir" << endl;
+        }
 	}
 
 	return parser;
@@ -132,7 +118,8 @@ bool checkCmdLine(int argc, char ** argv, Args& args)
 {
     args = Args();
 
-    if(argc <= 1) {
+    if(argc <= 1)
+    {
         cout << USAGE_MESSAGE << endl;
         return false;
     }
@@ -142,7 +129,8 @@ bool checkCmdLine(int argc, char ** argv, Args& args)
     {
         switch(c)
         {
-            case 'g': {
+            case 'g':
+            {
                 args.makeGraph = true;
                 args.graphOutputFileName = string(optarg);
                 break;
@@ -150,43 +138,52 @@ bool checkCmdLine(int argc, char ** argv, Args& args)
             case 'e':
                 args.blacklistFiles = true;
                 break;
-            case 't': {
+            case 't':
+            {
                 args.filterHour = true;
                 bool gotBadT = false;
                 size_t argLen = strlen(optarg);
-                if (argLen <= 2) {
-                    for (int j = 0; j < argLen && !gotBadT; ++j) {
-                        if (optarg[0] <= 48 || optarg[0] >= 57) {
+                if (argLen <= 2)
+                {
+                    for (int j = 0; j < argLen && !gotBadT; ++j)
+                    {
+                        if (optarg[0] <= 48 || optarg[0] >= 57)
                             gotBadT = true;
-                        }
                     }
-                    if (!gotBadT) {
+                    if (!gotBadT)
+                    {
                         args.filterHour = true;
                         args.hour = atoi(optarg);
                         if (args.hour < 0 || args.hour > 23)
                             gotBadT = true;
                     }
-                } else {
+                }
+                else
+                {
                     gotBadT = true;
                 }
                 // got bad or no number after -t
-                if (gotBadT) {
+                if (gotBadT)
+                {
                     cout << "Argument -t expects a number between 0 and 23" << endl;
                     return false;
                 }
                 break;
             }
             case '?':
-                if(optopt == 't') {
+                if(optopt == 't')
+                {
                     cout << "Argument -t expects a number between 0 and 23" << endl;
                     return false;
                 }
-                else if(optopt == 'g') {
+                else if(optopt == 'g')
+                {
                     args.makeGraph = true;
                     args.graphOutputFileName = string("out.dot");
                     break;
                 }
-                else {
+                else
+                {
                     if(isprint(c))
                         cout << "Unknown argument : -" << char(optopt) << endl;
                     else
@@ -199,10 +196,10 @@ bool checkCmdLine(int argc, char ** argv, Args& args)
         }
     }
 
-    if(optind < argc) {
+    if(optind < argc)
         args.inputFileName = argv[optind];
-    }
-    else {
+    else
+    {
         cout << USAGE_MESSAGE << endl;
         return false;
     }
@@ -210,7 +207,12 @@ bool checkCmdLine(int argc, char ** argv, Args& args)
 	return true;
 } //----- Fin de checkCmdLine
 
-void mkTopTen( Parser& p)
+static bool cmp (pair<string, int> const & a, pair<string, int> const & b)
+{
+    return a.second > b.second;
+}
+
+void mkTopTen( Parser& p )
 // Algorithme :
 //
 {
@@ -241,7 +243,8 @@ void mkTopTen( Parser& p)
 	//Trie les éléments
 	vector<pair<string, int>> connections_sorted;
 
-	for(map<string, int>::const_iterator i = connections.begin(); i!=connections.end(); i ++){
+	for(map<string, int>::const_iterator i = connections.begin(); i!=connections.end(); i ++)
+	{
 		connections_sorted.emplace_back(*i);
 	}
 
@@ -251,17 +254,21 @@ void mkTopTen( Parser& p)
 	vector<pair<string, int>>::const_iterator aff = connections_sorted.begin();
 
 	//Moins de 10 logs
-	if(connections_sorted.size() < 10){
-		for(; aff != connections_sorted.end(); aff ++){
+	if(connections_sorted.size() < 10)
+	{
+		for(; aff != connections_sorted.end(); aff ++)
+		{
 			cout << left << setw(50) << aff->first << " ( " << aff->second << " hits )" << endl;
 		}
 	}
 
 	//Plus de 10 logs
-	else{
+	else
+    {
 		int cpt = 0;
 		int max = aff->second;
-		for(size_t i = 0; i<10; i++, aff ++){
+		for(size_t i = 0; i<10; i++, aff ++)
+		{
 			if(aff->second == max)
 			{
 				cpt++;
@@ -290,7 +297,8 @@ void mkGraph( Parser& parser, const string& outputFileName )
         parser.nextLine();
 
         const string& referer = *parser.get(Parser::REFERER);
-        if(referer != "-") {
+        if(referer != "-")
+        {
             // trouver l'indice de début du nom d'hôte dans l'url
             size_t pos1 = referer.find("//", 0) + 2; // +2 pour passer compter les //
 
@@ -300,16 +308,20 @@ void mkGraph( Parser& parser, const string& outputFileName )
             //hTODO maybe simplify really long urls (like google ones for example)
 
             // l'url contient une page (au moins un / à la fin)
-            if(pos2 != string::npos) {
+            if(pos2 != string::npos)
+            {
                 // le nom d'hôte correspond au nom d'hôte local
-                if(referer.substr(pos1, pos2 - pos1).rfind(LOCAL_URL) != string::npos) {
+                if(referer.substr(pos1, pos2 - pos1).rfind(LOCAL_URL) != string::npos)
+                {
                     graph.add(referer.substr(pos2, referer.size()), *parser.get(Parser::DOCUMENT));
                 }
-                else {
+                else
+                {
                     graph.add(referer, *parser.get(Parser::DOCUMENT));
                 }
             }
-            else {
+            else
+            {
                 graph.add(referer, *parser.get(Parser::DOCUMENT));
             }
         }
